78d10ec739e94b9d3449b1d97c7a7169f655f82b
iago-f-s-e <iagof.elesbao@gmail.com>
2023-09-17 19:59:45 -0300

    refactor: exports types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export { CatchException, LoggerService } from './lib';
+export type { CatchExceptionOptions } from './lib';
+export { CatchException, catchException, LoggerService } from './lib';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+export type { CatchExceptionOptions } from './core/types';
 export { CatchException } from '@core/decorators';
 export { catchException } from '@core/functions';
 export { LoggerService } from '@core/services';
